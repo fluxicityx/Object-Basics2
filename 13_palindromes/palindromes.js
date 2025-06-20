b7d8d3e3d68aad15f37ef62ab58188c85bc5856d
RockPaperScissors <malikmouthif576@gmail.com>
2025-06-20 13:32:46 -0400

    Object

---
+++ 13_palindromes/palindromes.js
@@ -0,0 +1,15 @@
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+      const cleanedString = string.toLowerCase().split('').filter((character) => alphanumerical.includes(character)).join('');
+    
+       const reversedString = cleanedString.split('').reverse().join('');
+
+        return cleanedString === reversedString;
+    };
+
+    palindromes("racecar")
+    palindromes("banana")
+    palindromes("A car, a man, a maraca")
+// Do not edit below this line
+module.exports = palindromes;
